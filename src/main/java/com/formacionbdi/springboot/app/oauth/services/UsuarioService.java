0d9d5c8fa5d0cb07530ae613b54928edc4d003ac
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-07-03 10:27:18 +0200

    Implemented UserDetailsService with Feign Client

---
+++ src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
@@ -0,0 +1,47 @@
+package com.formacionbdi.springboot.app.oauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
+import com.formacionbdi.springboot.app.oauth.clients.UsuarioFeignClient;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UsuarioService implements UserDetailsService {
+
+	private Logger log = LoggerFactory.getLogger(UsuarioService.class);
+	
+	@Autowired
+	private UsuarioFeignClient client;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = client.findByUsername(username);
+		
+		if(usuario == null) {
+			log.error("Error en el login, no existe el usuario '" + username + "' en el sistema");
+			throw new UsernameNotFoundException("Error en el login, no existe el usuario '" + username + "' en el sistema");
+		}
+		
+		List<GrantedAuthority> authorities = usuario.getRoles()
+				.stream()
+				.map(role -> new SimpleGrantedAuthority(role.getNombre()))
+				.peek(authority -> log.info("Role: " + authority.getAuthority()))
+				.collect(Collectors.toList());
+		
+		log.info("Usuario autenticado: " + username);
+		
+		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true, authorities);
+	}
+
+}
